2fd1ddd87b90735e1434d1a14676f66b0403a502
ShadmanKhan10 <khanshadman7777@gmail.com>
2024-08-10 13:03:43 +0530

    made a simple todo in free time

--- 10-08-2024/src/App.jsx
+++ 10-08-2024/src/App.jsx
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleTodoChange = (event) => {
+    setTodo(event.target.value);
+  };
+
+  const setTask = () => {
+    setTodos((prev) => [...prev, todo]);
+    console.log("Tasks clicked", todos);
+  };
+
+  const deleteTodo = (index) => {
+    setTodos((prevTodos) => prevTodos.filter((todo, i) => i !== index));
+  };
+
+  useEffect(() => {
+    console.log("Updated tasks:", todos);
+  }, [todos]);
   return (
     <>
-      <h1>Ready for a new Project</h1>
+      <h1>ToDo List Project</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="settodo"
+          value={todo}
+          onChange={handleTodoChange}
+        />
+        <button onClick={setTask}>SetTodo</button>
+      </div>
+      {todos.map((to, index) => (
+        <div key={index}>
+          <p>{to}</p>
+          <button
+            onClick={() => {
+              deleteTodo(index);
+            }}
+          >
+            Delete
+          </button>
+        </div>
+      ))}
     </>
   );
 }
